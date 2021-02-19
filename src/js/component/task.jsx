8789cc6f27446cbe34aaa64b0abbf506a6020150
YARA-G <y.gomez.saiz@hotmail.com>
2021-02-19 18:55:10 +0000

    color change

--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -16,10 +16,8 @@ export default function Task(props) {
 	return (
 		<li className="list-group-item showitem">
 			{" Task "}
-			{props.index + 1}
-			{" : "}
-
-			{props.listitem}
+			{props.index + 1 + " : "}
+			{props.listitem + "     "}
 			{/* a la clase se le selecciona en css visibilidad con hover
             de forma que cuando pongas el raton encima muestre un boton para borrar tarea */}
 			<button className="DeleteButton show" onClick={callDelete}>
